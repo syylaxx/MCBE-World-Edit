72f0290db63dc165e822a37f303e6a9c520d37cb
syylaxx <agatobik1982@gmail.com>
2024-01-30 16:39:55 +0100

    Place Block
    Added Place Block Function.

---
+++ BP/scripts/World Edit/PlaceBlock.js
@@ -0,0 +1,21 @@
+import { world, system, Vector } from "@minecraft/server"
+import "Wrapper/Wrapper.js"
+
+world.beforeEvents.itemUse.subscribe((eventData) => {
+    const { source, itemStack } = eventData
+
+    if (itemStack?.typeId !== "world-edit:axe")
+        return
+
+    if (!source.isFlying && !source.isFalling)
+        return
+
+    system.run(() => {
+        try {
+            world.getDimension("overworld").fillBlocks(new Vector(source.location.x, source.location.y - 1, source.location.z), new Vector(source.location.x, source.location.y - 1, source.location.z), "minecraft:bedrock")
+            source.sendMessage("§l§8 » §r§7Successfully placed Block!§r")
+        } catch (error) {
+            source.sendMessage("§l§8 » §r§7Placing Block failed! It can be Out of the World or wrong TypeID!§r")
+        }
+    })
+})
--- BP/scripts/import.js
+++ BP/scripts/import.js
@@ -3,3 +3,4 @@ import "./Chat/Commands/fill.js"
 import "./Chat/Commands/prefix.js"
 
 import "./World Edit/CoordinateSystem.js"
+import "./World Edit/PlaceBlock.js"
