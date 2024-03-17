f03232262c2b7012cbea8888d81f34df2b6e60b0
syylaxx <123977419+syylaxx@users.noreply.github.com>
2024-03-17 20:50:38 +0100

    Added Sphere Feature

--- BP/scripts/Chat/Commands/sphere.js
+++ BP/scripts/Chat/Commands/sphere.js
@@ -1,12 +1,55 @@
-import { world, Vector } from "@minecraft/server"
+import { world, Vector, BlockPermutation, system } from "@minecraft/server"
 
 /**
- * @param {number} radius
- * @param {Vector} vector
+ * 
+ * @param {Vector} coordinate
+ * @param {number} radius 
+ * @param {string} blockType 
  */
 
-function sphere(radius, vector) {
+function createSphere(coordinate, radius, blockType, player) {
+    const { x: centerX, y: centerY, z: centerZ } = coordinate;
 
+    const maxBlocksPerInterval = world.getDynamicProperty("WorldEdit:SphereCreationSpeed")
+    const delayBetweenIntervals = 1; // Opóźnienie między kolejnymi interwałami (w tickach)
+
+    let xIndex = centerX - radius;
+    let yIndex = centerY - radius;
+    let zIndex = centerZ - radius;
+
+    const run = system.runInterval(() => {
+        for (let i = 0; i < maxBlocksPerInterval; i++) {
+            if (xIndex <= centerX + radius && yIndex <= centerY + radius && zIndex <= centerZ + radius) {
+                const distance = Math.sqrt(Math.pow(xIndex - centerX, 2) + Math.pow(yIndex - centerY, 2) + Math.pow(zIndex - centerZ, 2));
+                if (distance <= radius) {
+                    const vector = new Vector(xIndex, yIndex, zIndex);
+                    try {
+                        //world.getDimension('overworld').getBlock(vector).setPermutation(BlockPermutation.resolve(blockType));
+                        world.getDimension('overworld').fillBlocks(vector, vector, blockType)
+                    } catch (error) {
+                        player.sendMessage("§l§8 » §r§7Creating Sphere failed! Wrong typeID or out of World!§r");
+                        
+                        system.clearRun(run);
+                        return;
+                    }
+                }
+
+                zIndex++;
+                if (zIndex > centerZ + radius) {
+                    zIndex = centerZ - radius;
+                    yIndex++;
+                }
+                if (yIndex > centerY + radius) {
+                    yIndex = centerY - radius;
+                    xIndex++;
+                }
+            } else {
+                player.sendMessage("§l§8 » §r§7Successfully Created a Sphere!§r");
+                system.clearRun(run);
+                return;
+            }
+        }
+    }, delayBetweenIntervals);
 }
 
 /**
@@ -14,17 +57,14 @@ function sphere(radius, vector) {
  * @param {string[]} messageParts
  */
 
-function createSphere(player, messageParts) {
+function sphere(player, messageParts) {
     const coordinate1 = player.getDynamicProperty("WorldEdit:Coordinate1")
-    const coordinate2 = player.getDynamicProperty("WorldEdit:Coordinate1")
+    const coordinate2 = player.getDynamicProperty("WorldEdit:Coordinate2")
 
     if (coordinate1 === undefined) {
         player.sendMessage("§l§8 » §r§7To execute this Command you need atleast One Saved Block!§r")
         return
     }
-
-    const coordinates1 = coordinate1.x + " " + coordinate1.y + " " + coordinate1.z
-    const coordinates2 = coordinate2.x + " " + coordinate2.y + " " + coordinate2.z
 
     let coordinate
 
@@ -36,8 +76,15 @@ function createSphere(player, messageParts) {
             coordinate = coordinate2
     }
 
+    let sphereID = messageParts[1]
+
+    if (!sphereID.startsWith("minecraft:")) 
+        sphereID = "minecraft:" + messageParts[1]
+
+    const r = messageParts[2]
+    
     try {
-        sphere(r, coordinate)
+        createSphere(coordinate, Number(r), sphereID, player)
         player.sendMessage("§l§8 » §r§7Successfully Created a Sphere!§r")
     } catch (error) {
         player.sendMessage("§l§8 » §r§7Creating Sphere failed! It can be Too Big or out of World!§r")
@@ -55,5 +102,17 @@ world.beforeEvents.chatSend.subscribe((eventData) => {
     if (!message.toLowerCase().startsWith(prefix + "sphere "))
         return
 
-    createSphere(sender, message.split(" "))
+    eventData.cancel = true
+
+    if (message.toLowerCase() === (prefix + "sphere"))
+        return sender.sendMessage("§l§8 » §r§7Command was incorrect!\n§l§8 » §r§7 " + prefix + "sphere <typeID> <radius>§r")
+
+    sphere(sender, message.split(" "))
+})
+
+world.afterEvents.worldInitialize.subscribe(() => {
+    if (world.getDynamicProperty("WorldEdit:SphereCreationSpeed"))
+        return
+
+    world.setDynamicProperty("WorldEdit:SphereCreationSpeed", 1500)
 })
